fbe19c39543214248c2b92450852ec8f89d64274
Simon Stubben <simon.stubben@lionandlion.com>
2015-05-26 18:16:14 +0800

    added isset

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 	function contact(){
 		$result = '';
 
-		if ($_POST['name']) {
+		if (isset($_POST['submit'])) {
 			$name = $_POST['name'];
 			$email = $_POST['email'];
 			$message = $_POST['message'];
